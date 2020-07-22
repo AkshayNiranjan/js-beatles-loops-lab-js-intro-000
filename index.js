314e24b209cf405561b532776ddba1d898f5e5ef
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-22 03:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(musicians,instruments){
   var arr=[]
   for(let i=0;i<musicians.length;i++){
-    arr.push(``)
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 }
