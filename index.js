acb8cb017ad8324eec6307e41be9b58d316a6647
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-22 03:34:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  var arr=[]
+  var arr=new Array()
   for(let i=0;i<musicians.length;i++){
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return arr
 }
 function johnLennonFacts(facts){
-  var arr2=[]
+  var arr2=new Array()
   let i=0
   while(i<facts.length){
     arr2.push(facts[i++]+"!!!")
